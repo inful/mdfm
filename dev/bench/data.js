9117dc6915d8d4ae3c8bad6313c9f4860432c49e
github-action-benchmark <github@users.noreply.github.com>
2026-03-17 21:15:22 +0000

    add mdfm Go Benchmarks (go) benchmark result for c758db140374ee2b754bc4bc74b33c63a5ef691d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773781536656,
+  "lastUpdate": 1773782122068,
   "repoUrl": "https://github.com/inful/mdfm",
   "entries": {
     "mdfm Go Benchmarks": [
@@ -72,6 +72,78 @@ window.BENCHMARK_DATA = {
             "value": 170,
             "unit": "allocs/op",
             "extra": "45649 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "inful@users.noreply.github.com",
+            "name": "Jone Marius Vignes",
+            "username": "inful"
+          },
+          "committer": {
+            "email": "inful@users.noreply.github.com",
+            "name": "Jone Marius Vignes",
+            "username": "inful"
+          },
+          "distinct": true,
+          "id": "c758db140374ee2b754bc4bc74b33c63a5ef691d",
+          "message": "test: push coverage above eighty percent",
+          "timestamp": "2026-03-17T21:13:14Z",
+          "tree_id": "adfbec1d5f036a0379b0e700bb4501ae98107255",
+          "url": "https://github.com/inful/mdfm/commit/c758db140374ee2b754bc4bc74b33c63a5ef691d"
+        },
+        "date": 1773782120623,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkParse",
+            "value": 10823,
+            "unit": "ns/op\t   10872 B/op\t      90 allocs/op",
+            "extra": "113372 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParse - ns/op",
+            "value": 10823,
+            "unit": "ns/op",
+            "extra": "113372 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParse - B/op",
+            "value": 10872,
+            "unit": "B/op",
+            "extra": "113372 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParse - allocs/op",
+            "value": 90,
+            "unit": "allocs/op",
+            "extra": "113372 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSetAndBytes",
+            "value": 25346,
+            "unit": "ns/op\t   39748 B/op\t     170 allocs/op",
+            "extra": "48320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSetAndBytes - ns/op",
+            "value": 25346,
+            "unit": "ns/op",
+            "extra": "48320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSetAndBytes - B/op",
+            "value": 39748,
+            "unit": "B/op",
+            "extra": "48320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSetAndBytes - allocs/op",
+            "value": 170,
+            "unit": "allocs/op",
+            "extra": "48320 times\n4 procs"
           }
         ]
       }
